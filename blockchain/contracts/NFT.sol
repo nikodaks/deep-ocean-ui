9d005f98a1505dc96d3bb258ba8e23f12aeb476d
Kirill Nikoda <knikodait@gmail.com>
2022-12-21 20:53:06 +0300

    feat: blockchain module initial setup

---
+++ blockchain/contracts/NFT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFT {
+
+}
